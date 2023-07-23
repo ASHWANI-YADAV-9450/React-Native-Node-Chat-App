616d89fc1434c914cd2c57222e18d0c9a09c9753
Ashwani Yadav <ashwaniyadav6779@gmail.com>
2023-07-23 09:28:06 +0530

    23rd July 2023

---
+++ screens/ChatMessagesScreen.js
@@ -0,0 +1,29 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  TextInput,
+  Pressable,
+  Image,
+} from "react-native";
+import React, {
+  useState,
+  useContext,
+  useLayoutEffect,
+  useEffect,
+  useRef,
+} from "react";
+
+const ChatMessagesScreen = () => {
+  return (
+    <View>
+      <Text>ChatMessagesScreen</Text>
+    </View>
+  );
+};
+
+export default ChatMessagesScreen;
+
+const styles = StyleSheet.create({});
